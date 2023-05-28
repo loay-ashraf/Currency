3c540de19e87c7bcda5043ea3bc148873bef0b29
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-05-28 22:15:29 +0300

    feat: add loading indicator to Currency Details View
    Resolves: none.

--- Currency/Modules/Currency Details/Presentation/View/Controller/CurrencyDetailsViewController.swift
+++ Currency/Modules/Currency Details/Presentation/View/Controller/CurrencyDetailsViewController.swift
@@ -10,79 +10,35 @@ import RxSwift
 import RxCocoa
 
 class CurrencyDetailsViewController: UIViewController {
-//    var baseCurrency: String?
-//    var targetCurrency: String?
-//    private let viewModel: CurrencyDetailsViewModel = {
-//        let dataSource = DefaultCurrencyDetailsRemoteDataSource(networkManager: .shared)
-//        let repository = DefaultCurrencyDetailsRepository(dataSource: dataSource)
-//        let fetchRateHistoryUseCase = DefaultFetchRateHistoryUseCase(repository: repository)
-//        let viewModel = CurrencyConverterViewModel(baseCurrency: baseCurrency ?? "EUR", targetCurrency: targetCurrency ?? "USD", fetchRateHistoryUseCase: fetchRateHistoryUseCase)
-//        return viewModel
-//    }()
     var viewModel: CurrencyDetailsViewModel?
     private let disposeBag = DisposeBag()
     @IBOutlet weak var rateHistoryTableView: UITableView!
-    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        setupUI()
-        rateHistoryTableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
+        setupUI()
         setupUIBindings()
         startLoading()
     }
+    private func setupUI() {
+        rateHistoryTableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
     private func setupUIBindings() {
-        setupInputBindings()
         setupOutputBindings()
     }
-    private func setupInputBindings() {
-//        fromCurrencyButton.rx.tap
-//            .bind(onNext: {
-//                self.toCurrencyPickerView.isHidden = true
-//                self.fromCurrencyPickerView.isHidden.toggle()
-//            })
-//            .disposed(by: disposeBag)
-//        toCurrencyButton.rx.tap
-//            .bind(onNext: {
-//                self.fromCurrencyPickerView.isHidden = true
-//                self.toCurrencyPickerView.isHidden.toggle()
-//            })
-//            .disposed(by: disposeBag)
-//        fromCurrencyPickerView.rx.modelSelected(String.self)
-//            .compactMap({ $0.first })
-//            .bind(to: viewModel.selectedBaseCurrency)
-//            .disposed(by: disposeBag)
-//        toCurrencyPickerView.rx.modelSelected(String.self)
-//            .compactMap({ $0.first })
-//            .bind(to: viewModel.selectedTargetCurrency)
-//            .disposed(by: disposeBag)
-//        fromCurrencyAmountTextField.rx
-//            .controlEvent(.editingChanged)
-//            .withLatestFrom(fromCurrencyAmountTextField.rx.text.orEmpty)
-//            .map({ $0.isEmpty ? "0.0" : $0 })
-//            .distinctUntilChanged()
-//            .debounce(.milliseconds(100), scheduler: MainScheduler.instance)
-//            .compactMap({ Double($0) })
-//            .do(onNext: { print($0) })
-//            .bind(to: viewModel.baseCurrencyAmountInput)
-//            .disposed(by: disposeBag)
-//        toCurrencyAmountTextField.rx
-//            .controlEvent(.editingChanged)
-//            .withLatestFrom(toCurrencyAmountTextField.rx.text.orEmpty)
-//            .map({ $0.isEmpty ? "0.0" : $0 })
-//            .distinctUntilChanged()
-//            .debounce(.milliseconds(100), scheduler: MainScheduler.instance)
-//            .compactMap({ Double($0) })
-//            .do(onNext: { print($0) })
-//            .bind(to: viewModel.targetCurrencyAmountInput)
-//            .disposed(by: disposeBag)
-//        swapCurrencyButton.rx.tap
-//            .bind(onNext: {
-//                self.swapCurrenciesAction()
-//            })
-//            .disposed(by: disposeBag)
-    }
     private func setupOutputBindings() {
+        viewModel?.viewState
+            .map({ [.loading(loadType: .initial), .loading(loadType: .baseDriven), .loading(loadType: .targetDriven)].contains($0) })
+            .bind(to: loadingIndicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+        viewModel?.error
+            .drive(onNext: {
+                let alertController = UIAlertController(title: "Error", message: "An error occured.\n\($0.localizedDescription)", preferredStyle: .alert)
+                alertController.addAction(.init(title: "Ok", style: .default))
+                self.present(alertController, animated: true)
+            })
+            .disposed(by: disposeBag)
         viewModel?.rateHistory
             .drive(rateHistoryTableView.rx.items(cellIdentifier: "Cell", cellType: TableViewCell.self)) { (row, item, cell) in
                 cell.textLabel?.text = item.date
@@ -98,6 +54,7 @@ class CurrencyDetailsViewController: UIViewController {
 class TableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
